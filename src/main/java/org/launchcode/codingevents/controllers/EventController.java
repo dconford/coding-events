1d08f6901b36ecc31118481498ad8e8c7c988995
Daniel C <daniel@daniel.com>
2019-12-14 13:56:12 -0600

    added create form, committing after end of video.

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,34 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    //lives @ events/index
+    @GetMapping
+    public String displayAllEvents (Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("Code With Pride");
+        events.add("StrangeLoop");
+        events.add("Apple WWDC");
+        events.add("SpringOne Platform");
+        events.add("Geek Gala 2020");
+        events.add("MoonShot Awards 2020");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //lives@ events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+}
